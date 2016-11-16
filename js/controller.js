751a8bb25feb2bcd36b0ad0408916d8e09a1e7d5
Kirill <kam4513@gmail.com>
2016-11-16 19:21:56 +0200

    initial commit

---
+++ js/controller.js
@@ -0,0 +1,30 @@
+app.controller('MapCtrl', ['NgMap','$scope', function (NgMap, $scope) {
+    var vm = this;
+    vm.types = "['establishment']";
+    vm.placeChanged = function () {
+        vm.place = this.getPlace();
+        $scope.places.push({
+            text: vm.place.formatted_address,
+            loc: vm.place.geometry.location
+        });
+        localStorage.setItem('places', JSON.stringify($scope.places));
+        vm.map.setCenter(vm.place.geometry.location);
+    };
+
+    $scope.changePlace = function (city) {
+        vm.map.setCenter(city.loc);
+    };
+
+    NgMap.getMap().then(function (map) {
+        vm.map = map;
+    });
+
+    $scope.saved = localStorage.getItem('places');
+    $scope.places = (localStorage.getItem('places') !== null) ? JSON.parse($scope.saved) : [];
+    localStorage.setItem('places', JSON.stringify($scope.places));
+    $scope.remove = function (place) {
+        var index = $scope.places.indexOf(place);
+        $scope.places.splice(index, 1);
+        localStorage.setItem('places', JSON.stringify($scope.places));
+    };
+}]);
